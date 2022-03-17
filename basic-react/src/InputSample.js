b574c4ecba86ec029802a428e998a58c6a7c6795
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-03-17 23:55:45 +0900

    Feat: add UserList.js

--- basic-react/src/InputSample.js
+++ basic-react/src/InputSample.js
@@ -1,20 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample() {
-  const [text, setText] = useState("");
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+  });
+
+  const nameInput = useRef();
+
+  const { name, nickname } = inputs; // 비구조화 할당으로 값 추출
+
   const onChange = (e) => {
-    setText(e.target.value);
+    const { value, name } = e.target; // 먼저, e.target에서 name과 value 추출
+    setInputs({
+      ...inputs, // 기존의 inputs 객체 복사
+      [name]: value, // name키를 가진 값을 value로 설정
+    });
   };
 
-  const onReset = (e) => {
-    setText("");
+  const onReset = () => {
+    setInputs({
+      name: "",
+      nickname: "",
+    });
+    nameInput.current.focus();
   };
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name}({nickname})
       </div>
     </div>
   );
