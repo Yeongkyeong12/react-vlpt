89e60e2d35ed3d4945c74286b32a8752c99c96aa
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-03-25 23:22:59 +0900

    Feat: add CreateUser.js

--- basic-react/src/UserList..js
+++ basic-react/src/UserList..js
@@ -8,29 +8,13 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-    },
-  ];
+function UserList({ users }) {
   return (
     <div>
-      <User user={users[0]} />
-      <User user={users[1]} />
-      <User user={users[2]} />
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+        // 배열 렌더링에서 고유한 key값은 필수!
+      ))}
     </div>
   );
 }
